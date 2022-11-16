f1792a8cea7c3668ee1a94cb3caa83a556561214
Kyubin30 <leekebin3@naver.com>
2022-11-16 17:33:03 +0900

    기초 뼈대

--- python/create_dataset.py
+++ python/create_dataset.py
@@ -1,12 +1,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-import time, os
+import os
+import csv
 
-#exer1: 팔굽혀펴기
-actions = ['exr1']
-seq_length = 30
-secs_for_action = 30
+'''
+push-up: 팔굽혀펴기-편 동작, 
+push-down: 팔굽혀펴기-다운 동작
+'''
+actions = ['push-up', 'push-down']
 
 #Mediapipe pose model
 mp_pose = mp.solutions.pose
@@ -17,7 +19,18 @@ pose = mp_pose.Pose(
     min_tracking_confidence=0.5
 )
 
-cap = cv2.VideoCapture(0)
+#AI-Hub에서 피트니스 이미지 데이터를 사용할 계획
+#운동의 각 구분동작 별로 데이터셋 폴더 나눔
+#knn알고리즘으로 각 구분 동작 학습
 
-created_time = int(time.time())
-os.makedirs('dataset', exist_ok=True)
+#csv는 반복 전 열어 각 반복마다 학습한 값과 라벨을 각 row에 저장
+dataset = open("dataset.csv", "w")
+
+#actions의 크기만큼 반복
+for action in actions:
+    #actions의 각 명칭별로 있는 폴더 안의 이미지 사용
+    img_path = "./" + action
+    file_list = os.listdir(img_path)
+    for img_file in file_list:
+        #각 이미지 별로 포즈 학습
+        img = cv2.imread('./' + img_path + '/' + img_file, cv2.IMREAD_COLOR)
