2ed8dce53a8a5bad885d02489dd7c1c834e9c057
Kyubin30 <leekebin3@naver.com>
2022-11-17 15:25:03 +0900

    normalizing 전 코드, gitignore 수정

--- python/create_dataset.py
+++ python/create_dataset.py
@@ -5,8 +5,8 @@ import os
 import csv
 
 '''
-push-up: 팔굽혀펴기-편 동작, 
-push-down: 팔굽혀펴기-다운 동작
+pushUp: 팔굽혀펴기-편 동작, 
+pushDown: 팔굽혀펴기-다운 동작
 '''
 actions = ['pushUp', 'pushDown']
 
@@ -17,8 +17,7 @@ color_pose2 = (245,66,230)
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 pose = mp_pose.Pose(
-    model_complexity=2,
-    min_detection_confiendce=0.5,
+    min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
 
@@ -32,22 +31,22 @@ dataset = open("dataset.csv", "w")
 #actions의 크기만큼 반복
 for action in actions:
     #actions의 각 명칭별로 있는 폴더 안의 이미지 사용
-    img_path = "./" + action
+    img_path = "python/data/" + action + '/'
     file_list = os.listdir(img_path)
     for img_file in file_list:
         #각 이미지 별로 포즈 학습
-        img = cv2.imread('./' + img_path + '/' + img_file, cv2.IMREAD_COLOR)
-
-        cv2.imshow('img', img)
-        
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.imread(img_path + img_file, cv2.IMREAD_COLOR)
 
         result = pose.process(img)
 
         mp_drawing.draw_landmarks(
-                    img, result.pose_landmarks, pose.POSE_CONNECTIONS, 
+                    img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
                     mp_drawing.DrawingSpec(color=color_pose1, thickness=2, circle_radius=4),
                     mp_drawing.DrawingSpec(color=color_pose2, thickness=2, circle_radius=2)
                 )
 
-        
+        if result.pose_landmarks is not None:
+            print(result.pose_landmarks)
+
+        cv2.imshow('img', img)
+        cv2.waitKey(10000)
