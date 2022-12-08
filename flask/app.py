dac18bfaf8b9d64ab8a795400a27dfff4f95ec78
Kyubin30 <leekebin3@naver.com>
2022-12-09 02:27:21 +0900

    변수 dict형식 변경, bootstrap 적용

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,12 @@
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-exerciseList = ["팔굽혀펴기", "크런치"]
+exerciseList = {
+    "pushup" : "팔굽혀펴기",
+    "crunch" : "크런치"
+}
 
 #초기 리스트 페이지
 @app.route("/")
@@ -13,9 +16,9 @@ def homepage():
 #get을 이용하여 리스트 페이지의 운동 이름을 가져옴
 @app.route("/detail", methods=['GET']) 		
 def hello_request():
-    data = int(request.args.get('exercise'))
+    data = request.args.get('exercise')
     #jsonify(data)
-    return render_template("detailPage.html", exercise = exerciseList[data])
+    return render_template("detailPage.html", exerciseEng = data, exerciseKor = exerciseList[data])
 
 if __name__ == '__main__':
     app.run(debug=True)
