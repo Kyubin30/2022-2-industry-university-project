6e3aab981f9fe17273690bc70ab580de8bab3209
Kyubin30 <leekebin3@naver.com>
2022-12-09 01:51:39 +0900

    GET방식 상세 페이지 작성 1

--- flask/first.py
+++ flask/first.py
@@ -1,12 +1,19 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
-
+#초기 리스트 페이지
 @app.route("/")
 def homepage():
    return 'coeni'
+
+#get을 이용하여 리스트 페이지의 운동 이름을 가져옴
+@app.route("/detail", methods=['GET']) 		
+def hello_request():
+    data = request.args.get('exercise')
+    jsonify(data)
+    return render_template("detailPage.html", exercise = data)
 
 if __name__ == '__main__':
     app.run(debug=True)
