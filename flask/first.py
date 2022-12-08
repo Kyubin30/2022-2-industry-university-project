904eb20565160ba57562059e38c08b41fce13339
Kyubin30 <leekebin3@naver.com>
2022-12-09 02:03:57 +0900

    코드 재사용성 증가

--- flask/first.py
+++ flask/first.py
@@ -1,7 +1,9 @@
 
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
+exerciseList = ["팔굽혀펴기", "크런치"]
 
 #초기 리스트 페이지
 @app.route("/")
@@ -11,9 +13,9 @@ def homepage():
 #get을 이용하여 리스트 페이지의 운동 이름을 가져옴
 @app.route("/detail", methods=['GET']) 		
 def hello_request():
-    data = request.args.get('exercise')
-    jsonify(data)
-    return render_template("detailPage.html", exercise = data)
+    data = int(request.args.get('exercise'))
+    #jsonify(data)
+    return render_template("detailPage.html", exercise = exerciseList[data])
 
 if __name__ == '__main__':
     app.run(debug=True)
